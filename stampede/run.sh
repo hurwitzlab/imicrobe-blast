b63c722a608c3268f18943bd64bb422170f1206a
Ken Youens-Clark <kyclark@gmail.com>
2017-02-22 18:07:01 -0600

    basically working

--- stampede/run.sh
+++ stampede/run.sh
@@ -9,11 +9,12 @@ QUERY=""
 PCT_ID=".98"
 OUT_DIR="$BIN"
 NUM_THREADS=12
+MAX_SEQS_PER_FILE=25
 
 module load blast
 
 function lc() {
-  wc -l "$1" | cut -d ' ' -f 1
+  wc -l "$1" | awk '{print $1}'
 }
 
 function HELP() {
@@ -108,19 +109,33 @@ if [[ $NUM_INPUT -lt 1 ]]; then
   exit 1
 fi
 
-# Here is a place for fasplit.py to ensure not too 
-# many sequences in each query.
+SPLIT_DIR="$OUT_DIR/split"
+if [[ ! -d $SPLIT_DIR ]]; then
+  mkdir -p $SPLIT_DIR
+fi
 
-BLAST_DIR="$WORK/ohana/blast"
+while read FILE; do
+  fasplit.py -f $FILE -o $SPLIT_DIR -n $MAX_SEQS_PER_FILE
+done < $INPUT_FILES
+
+SPLIT_FILES=$(mktemp)
+find $SPLIT_DIR -type f -size +0c > $SPLIT_FILES
+NUM_SPLIT=$(lc $SPLIT_FILES)
+
+echo "After splitting, there are NUM_SPLIT \"$NUM_SPLIT\""
+
+BLAST_DIR="$WORK/imicrobe/blast"
 
 if [[ ! -d "$BLAST_DIR" ]]; then
   echo "BLAST_DIR \"$BLAST_DIR\" does not exist."
   exit 1
 fi
 
-BLAST_DIR="$WORK/ohana/blast"
 BLAST_ARGS="-outfmt 6 -num_threads $NUM_THREADS"
 BLAST_PARAM="$$.blast.param"
+BLAST_DBS=$(mktemp)
+
+find $BLAST_DIR -type f -size +0c | sed "s/\..*//" | sort | uniq > $BLAST_DBS
 
 cat /dev/null > $BLAST_PARAM # make sure it's empty
 
@@ -155,28 +170,32 @@ while read INPUT_FILE; do
   fi
 
   if [[ ${#BLAST_TO_DNA} -gt 0 ]]; then
-    echo "$BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db $BLAST_DIR/contigs -query $INPUT_FILE -out $BLAST_OUT_DIR/$BASENAME-contigs.tab" >> $BLAST_PARAM
-    echo "$BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db $BLAST_DIR/genes -query $INPUT_FILE -out $BLAST_OUT_DIR/$BASENAME-genes.tab" >> $BLAST_PARAM
+    while read DB; do
+      BASE_DB=$(basename $DB)
+      echo "$BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db \"$BLAST_DIR/$BASE_DB\" -query \"$INPUT_FILE\" -out \"${BLAST_OUT_DIR}/${BASENAME}-${BASE_DB}.tab\"" >> $BLAST_PARAM
+    done < $BLAST_DBS
   fi
 
-  BLAST_TO_PROT=""
-  if [[ $TYPE == 'dna' ]]; then 
-    BLAST_TO_PROT='blastx'
-  elif [[ $TYPE == 'prot' ]]; then
-    BLAST_TO_PROT='blastp'
-  else
-    echo "Cannot BLAST $BASENAME to PROT (not DNA or prot)"
-  fi
+#  BLAST_TO_PROT=""
+#  if [[ $TYPE == 'dna' ]]; then 
+#    BLAST_TO_PROT='blastx'
+#  elif [[ $TYPE == 'prot' ]]; then
+#    BLAST_TO_PROT='blastp'
+#  else
+#    echo "Cannot BLAST $BASENAME to PROT (not DNA or prot)"
+#  fi
+#
+#  if [[ ${#BLAST_TO_PROT} -gt 0 ]]; then
+#    echo "$BLAST_TO_PROT $BLAST_ARGS -db $BLAST_DIR/proteins -query $INPUT_FILE -out $BLAST_OUT_DIR/$BASENAME-proteins.tab" >> $BLAST_PARAM
+#  fi
+done < "$SPLIT_FILES"
+rm "$SPLIT_FILES"
 
-  if [[ ${#BLAST_TO_PROT} -gt 0 ]]; then
-    echo "$BLAST_TO_PROT $BLAST_ARGS -db $BLAST_DIR/proteins -query $INPUT_FILE -out $BLAST_OUT_DIR/$BASENAME-proteins.tab" >> $BLAST_PARAM
-  fi
-done < "$INPUT_FILES"
-rm "$INPUT_FILES"
+NUM_JOBS=$(lc $PARAM)
 
-echo "Starting launcher for BLAST"
+echo "Starting launcher NUM_JOBS \"$NUM_JOBS\" for BLAST"
 export LAUNCHER_DIR="$HOME/src/launcher"
-export LAUNCHER_NJOBS=$(lc $BLAST_PARAM)
+export LAUNCHER_NJOBS=$NUM_JOBS
 export LAUNCHER_NHOSTS=4
 export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
 export LAUNCHER_WORKDIR=$BIN
@@ -186,30 +205,27 @@ export LAUNCHER_PPN=4
 export LAUNCHER_SCHED=interleaved
 $LAUNCHER_DIR/paramrun
 echo "Ended launcher for BLAST"
-
 rm $BLAST_PARAM
 
-# 
-# Now we need to add Eggnog (and eventually Pfam, KEGG, etc.)
-# annotations to the "*-genes.tab" files.
-# 
+#
+# Remove the empty files
+#
+find $BLAST_OUT_DIR -type f -size 0 -exec rm {} \;
+
+#
+# Add annotations
+#
 ANNOT_PARAM="$$.annot.param"
-cat /dev/null > $ANNOT_PARAM
 
-GENE_HITS=$(mktemp)
-find $BLAST_OUT_DIR -size +0c -name \*-genes.tab > $GENE_HITS
-while read FILE; do
-  BASENAME=$(basename $FILE '.tab')
-  echo "Annotating $FILE"
-  echo "annotate.py -b \"$FILE\" -a \"${WORK}/ohana/sqlite\" -o \"${OUT_DIR}/annotations\"" >> $ANNOT_PARAM
-done < $GENE_HITS
+echo "annotate.py -b \"$BLAST_OUT_DIR\" -a \"${WORK}/imicrobe/annotations/annots.db\" -o \"${OUT_DIR}/annotations\"" > $ANNOT_PARAM
 
-# Probably should run the above annotation with launcher, but I was 
-# having problems with this.
-echo "Starting launcher for annotation"
+NUM_JOBS=$(lc $ANNOT_PARAM)
+echo "Starting for annotation"
 export LAUNCHER_NHOSTS=1
-export LAUNCHER_NJOBS=$(lc $ANNOT_PARAM)
+export LAUNCHER_NJOBS=1
 export LAUNCHER_JOB_FILE=$ANNOT_PARAM
+export LAUNCHER_PPN=1
 $LAUNCHER_DIR/paramrun
 echo "Ended launcher for annotation"
+
 rm "$ANNOT_PARAM"
--- stampede/test.sh
+++ stampede/test.sh
@@ -4,15 +4,15 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -t 02:00:00
-#SBATCH -p normal
-#SBATCH -J mublast
+#SBATCH -p development
+#SBATCH -J imcrblst
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user kyclark@email.arizona.edu
 
-OUT_DIR="$SCRATCH/muscope-blast/test"
+OUT_DIR="$SCRATCH/imicrobe-blast/test"
 
 if [[ -d $OUT_DIR ]]; then
   rm -rf $OUT_DIR
 fi
 
-run.sh -q "$SCRATCH/muscope-blast/test.fa" -o $OUT_DIR
+run.sh -q "$SCRATCH/imicrobe-blast/test.fa" -o $OUT_DIR
