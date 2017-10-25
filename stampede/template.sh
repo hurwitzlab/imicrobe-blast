291fca16527884136cba4b82e07d14a1ac96141c
Ken Youens-Clark <kyclark@gmail.com>
2017-10-25 16:27:25 -0500

    so so close

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-echo "Started $(date)"
-ARGS="-o $(pwd)"
+echo "QUERY  \"${QUERY}\""
+echo "PCT_ID \"${PCT_ID}\""
 
-if [[ ${#PCT_ID} -gt 1 ]]; then
-  ARGS="$ARGS -p ${PCT_ID}"
-fi
-
-if [[ ${#OUT_DIR} -gt 1 ]]; then
-  ARGS="$ARGS -o ${OUT_DIR}"
-else
-  ARGS="$ARGS -o $(pwd)"
-fi
-
-sh run.sh -q ${QUERY} $ARGS
-echo "Ended $(date)"
+sh run.sh ${QUERY} ${PCT_ID}
--- stampede/test.sh
+++ stampede/test.sh
@@ -3,8 +3,8 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 02:00:00
-#SBATCH -p development
+#SBATCH -t 24:00:00
+#SBATCH -p normal
 #SBATCH -J imcrblst
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user kyclark@email.arizona.edu
