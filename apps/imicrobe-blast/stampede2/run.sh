5a0800e5c038d58f60514526fb72d34abfa2d428
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-16 18:26:31 -0400

    run BLAST from the imicrobe BLAST db directory

--- apps/imicrobe-blast/stampede2/run.sh
+++ apps/imicrobe-blast/stampede2/run.sh
@@ -25,7 +25,8 @@ PCT_ID=".98"
 OUT_DIR="$PWD/blast-out"
 #NUM_THREADS=48
 IMG="imicrobe-blast-0.0.5.img"
-BLAST_DB="/work/05066/imicrobe/iplantc.org/data/blast/one-db/imicrobe_blast_db"
+BLAST_DB_DIR="/work/05066/imicrobe/iplantc.org/data/blast/one-db"
+BLAST_DB="imicrobe_blast_db"
 #ANNOT_DB="/work/05066/imicrobe/iplantc.org/data/imicrobe-annotdb/annots.db"
 
 PARAMRUN="$TACC_LAUNCHER_DIR/paramrun"
@@ -198,7 +199,7 @@ while read -r SPLIT_FILE; do
         [[ ! -d "$HITS_DIR" ]] && mkdir -p "$HITS_DIR"
       
         #echo "$BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db $BLAST_DB -query $SPLIT_FILE -out $HITS_DIR/$SPLIT_NAME"
-        echo "$BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db $BLAST_DB -query $SPLIT_FILE -out $HITS_DIR/$SPLIT_NAME" >> "$BLAST_PARAM"
+        echo "cd $BLAST_DB_DIR; $BLAST_TO_DNA $BLAST_ARGS -perc_identity $PCT_ID -db $BLAST_DB -query $SPLIT_FILE -out $HITS_DIR/$SPLIT_NAME" >> "$BLAST_PARAM"
     fi
 done < "$SPLIT_FILES"
 
