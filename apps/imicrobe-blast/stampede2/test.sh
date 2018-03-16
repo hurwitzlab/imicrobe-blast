7bcb75f4f7a2c7708fef23f165b30774d195560c
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-16 14:22:46 -0400

    use different test data

--- apps/imicrobe-blast/stampede2/test.sh
+++ apps/imicrobe-blast/stampede2/test.sh
@@ -13,10 +13,8 @@ OUT_DIR="$SCRATCH/imicrobe-blast/test"
 
 [[ -d "$OUT_DIR" ]] && rm -rf "$OUT_DIR"
 
-DATA_DIR=/work/05066/imicrobe/iplantc.org/data/mmetsp/fasta
+DATA_DIR=/work/05066/imicrobe/iplantc.org/data/imicrobe/projects/37/samples
 
-# these data files are large
-# -rw------- 1 kyclark G-818024   54525952 Oct 10 12:36 1675-MMETSP0011_2-Rhodosorus-marinus-769.1.fa
-# -rw------- 1 kyclark G-818024    1638400 Oct  9 19:28 2283-MMETSP1452-Synchroma-pusillum-CCMP3072.1.fa
+# these data files are under 1MB
 
-sh run.sh -q "$DATA_DIR/2283-MMETSP1452-Synchroma-pusillum-CCMP3072.1.fa" -q "$DATA_DIR/1675-MMETSP0011_2-Rhodosorus-marinus-769.1.fa" -o $OUT_DIR
+sh run.sh -q "$DATA_DIR/739/ICOMM_SMPL_LTERSTATION1_REPLICATION1.fa" -q "$DATA_DIR/740/ICOMM_SMPL_LTERSTATION1_REPLICATION2.fa" -o $OUT_DIR
