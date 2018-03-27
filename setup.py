ccf3d12d0012f6bab15ad9aa963f9285a8a8e481
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-26 20:55:04 -0400

    update requirements specify -f to irods iget and iput

--- setup.py
+++ setup.py
@@ -70,7 +70,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'biopython'
+        'biopython',
+        'numpy'
     ],
 
     # List additional groups of dependencies here (e.g. development
