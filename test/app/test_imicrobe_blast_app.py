02e1ab7533edf7c00747defe43d25ba03e0ca75c
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2018-06-25 18:47:45 -0400

    initial commit

---
+++ test/app/test_imicrobe_blast_app.py
@@ -0,0 +1,32 @@
+import json
+
+import pytest
+
+from test import submit_job, wait_for_job_to_finish
+
+
+def test_imicrobe_blast_private(cyverse_oauth_session):
+    job_json = json.loads("""\
+        {
+          "name": "imicrobe-blast-test",
+          "appId": "imicrobe-blast-0.0.5",
+          "defaultMaxRunTime": "00:30:00",
+          "archive": true,
+          "inputs": {
+            "QUERY": "agave://data.iplantcollaborative.org/jklynch/data/muscope/last/test.fa"
+          },
+          "parameter": {
+          }
+        }
+    """)
+
+    submit_job_response = submit_job(
+        cyverse_oauth_session=cyverse_oauth_session,
+        job_json=job_json)
+    print(submit_job_response.json())
+
+    job_status = wait_for_job_to_finish(
+        job_id=submit_job_response.json()['result']['id'],
+        cyverse_oauth_session=cyverse_oauth_session)
+
+    assert job_status == 'FINISHED'
