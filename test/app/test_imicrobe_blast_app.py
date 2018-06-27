fef30a7200416f47973945762863b54588682754
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2018-06-27 17:07:49 -0400

    rename test directory from agave-app-test project

--- test/app/test_imicrobe_blast_app.py
+++ test/app/test_imicrobe_blast_app.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from test import submit_job, wait_for_job_to_finish
+from agave_app_test import submit_job, wait_for_job_to_finish
 
 
 def test_imicrobe_blast_private(cyverse_oauth_session):
